40b11fc810f94e14ddd8790953981878222a6930
= <=>
2022-07-31 16:15:27 +0200

    Add click on cell

--- SwiftUI-List/ContentView.swift
+++ SwiftUI-List/ContentView.swift
@@ -10,6 +10,8 @@ struct ContentView: View {
                 NavigationLink("Static List", destination: StaticListView()).foregroundColor(.red)
                 NavigationLink("Dynamic List", destination: DynamicListView()).foregroundColor(.orange)
                 NavigationLink("List With Action", destination: ListWithActionView()).foregroundColor(.yellow)
+                NavigationLink("Click on cell", destination: ClickOnCellView()).foregroundColor(.green)
+                
                 
             }.navigationTitle("Lists")
         }
